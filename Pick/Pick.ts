ef146408e1ead93279b0f82f10dcaa5b80a401d9
xy <598491063@qq.com>
2022-05-12 14:47:36 +0800

    add Pick.ts

---
+++ Pick/Pick.ts
@@ -0,0 +1,16 @@
+type MyPick<T, K extends keyof T> = {
+    [key in K]: T[key]
+}
+
+interface Todo {
+    title: string
+    description: string
+    completed: boolean
+}
+
+type TodoPreview = MyPick<Todo, 'title' | 'completed'>
+
+const todo: TodoPreview = {
+    title: 'clean room',
+    completed: false
+}
