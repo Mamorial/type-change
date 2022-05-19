28bcd3c88f9253cab4a1d504cb4a4321731b351e
xy <598491063@qq.com>
2022-05-19 16:37:23 +0800

    add GetReturnType.ts

---
+++ Medium/GetReturnType/GetReturnType.ts
@@ -0,0 +1,10 @@
+type MyReturnType<T> = T extends (...args: any[]) => infer P ? P : never
+
+
+const fn = (v: boolean) => {
+    if (v)
+        return 1
+    else
+        return 2
+}
+type Ak = MyReturnType<typeof fn> // 应推导出 "1 | 2"
